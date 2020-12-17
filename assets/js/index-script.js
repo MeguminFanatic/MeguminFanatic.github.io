1e8fdd6099b0f57f6f665a2e8155cd5a71ff1795
Megumin <66914211+MeguminFanatic@users.noreply.github.com>
2020-12-17 20:18:34 +0100

    Add files via upload

---
+++ assets/js/index-script.js
@@ -0,0 +1,25 @@
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickNavBar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+function refresh_dt()
+{
+	var ref=1000;
+	mytime=setTimeout('display_dt()',ref)
+}
+
+function display_dt()
+{
+	var d = new Date();
+	var t = new Date();
+	document.getElementById("date").innerHTML = (("0"+d.getDate()).slice(-2)) +"."+ (("0"+(d.getMonth()+1)).slice(-2)) +"."+ (d.getFullYear());
+	document.getElementById("time").innerHTML = (("0"+t.getHours()).slice(-2)) +":"+ (("0"+t.getMinutes()).slice(-2)) +":"+ (("0"+t.getSeconds()).slice(-2));
+	refresh_dt();
+}
